a3e53f92301a207566166aa336d6b8cbfc7c2b87
BuildTools <unconfigured@null.spigotmc.org>
2016-08-15 15:42:18 +0800

    Removal of Attack_type from elementl object

--- Base/Source/ElementalObject.h
+++ Base/Source/ElementalObject.h
@@ -4,15 +4,6 @@
 #include "GameObject.h"
 #include "BaseElement.h"
 
-enum ATTACK_TYPE
-{
-    NO_ATTACK_TYPE,//use this for non-interactable blocks
-    MELEE,
-    RANGED,
-    ABILITY,
-    TOTAL_ATTACK_TYPE,
-};
-
 class ElementalObject : public GameObject
 {
 public:
@@ -21,16 +12,11 @@ public:
 protected:
 
 	ElementalObject();
-
 	ELEMENT m_CurrElement;
-    ATTACK_TYPE m_CurrAttackType;
-
 	virtual ELEMENT GetElement();
-    virtual ATTACK_TYPE GetAttackType();
 	virtual void SetElement(ELEMENT SetElement);
 
-private:
-    virtual void SetAttackType();//a;ways corresponds to element type
+
 
 };
 
