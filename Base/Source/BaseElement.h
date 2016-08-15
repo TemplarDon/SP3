0f54174ea8de5baa86a661788eebb1643c01c616
BuildTools <unconfigured@null.spigotmc.org>
2016-08-15 11:52:54 +0800

    BaseElement class: Removed AIR Added secondary elements Added ATTACK_TYPE

--- Base/Source/BaseElement.h
+++ Base/Source/BaseElement.h
@@ -3,12 +3,29 @@
 
 enum ELEMENT
 {
-	NONE,
+	NO_ELEMENT,//use this for non-interactable blocks
+    //primary elements
 	FIRE,
 	EARTH,
 	WATER,
-	AIR,
+    //secondary elements
+    FIRE_2,
+    WATER_2,
+    EARTH_2,
+    //combined elements
+    SAND,// Fire + Earth
+    STEAM,//Water + Fire
+    WOOD,//Water+ Earth
+
 	TOTAL_ELEMENTS,
+};
+enum ATTACK_TYPE
+{
+    NO_TYPE,//use this for non-interactable blocks
+    MELEE,
+    RANGED,
+    ABILITY,
+    TOTAL_TYPE,
 };
 
 class BaseElement
@@ -19,11 +36,9 @@ public:
 protected:
 
 	ELEMENT m_CurrElement;
+    ATTACK_TYPE m_CurrType;
+	BaseElement(ELEMENT m_CurrElement = NO_ELEMENT, ATTACK_TYPE m_CurrType = NO_TYPE);
 
-	BaseElement(ELEMENT m_CurrElement = NONE);
-
-	virtual void Attack();
-	virtual void Ability();
 };
 
 #endif // !BASE_ELEMENT_H
