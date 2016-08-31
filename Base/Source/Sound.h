393013d426c7ee71a15802904ed61078b4224e3f
pohshania <pohshania@gmail.com>
2016-08-31 09:25:25 +0800

    Added sound manager

---
+++ Base/Source/Sound.h
@@ -0,0 +1,30 @@
+//#ifndef SOUND_H
+//#define SOUND_H
+//
+//#include "irrKlang.h"
+//#include <string>
+//using std::string;
+//
+//#pragma comment(lib, "irrKlang.lib")
+//
+//class Sound
+//{
+//public:
+//
+//	Sound();
+//	~Sound();
+//	void playMusic(string Music);
+//	void stopMusic(string Music);
+//	void setIsPaused(bool set);
+//	void dropMusic();
+//	void playSE(string Music, bool startpaused = false);
+//	bool getPaused();
+//	void play3dSound(string Music, irrklang::vec3df pos, irrklang::vec3df view, irrklang::vec3df targetpos);
+//
+//private:
+//	irrklang::ISoundEngine* musicEngine;
+//	irrklang::ISoundEngine* SoundEffect3D;
+//	irrklang::ISound* Song;
+//	irrklang::ISound* SoundEffect;
+//};
+//#endif
